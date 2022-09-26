4dc8524b489dbf4163776e8e40e240b0cb85840a
이름 <63229014+HyeM207@users.noreply.github.com>
2022-09-26 16:59:34 +0900

    Fix: Section.js

--- BlackNWhite-Server/schemas/roomTotal/Section.js
+++ BlackNWhite-Server/schemas/roomTotal/Section.js
@@ -7,17 +7,12 @@ const Section = new Schema({
     destroyStatus  : { type : Boolean, required : true },
     level  : { type : Number, required : true },
     suspicionCount : { type : Number, required : true },   // 회사 별 첫 취약점 공격 인덱스
-    defenseActive : { type : Array, required : true },
     attackProgress : { type : Array, required : true },
     defenseProgress : { type : Array, required : true },
-    // defenseStep : { type : Number, required : true },   // 수행해야 할 관제 단계를 뜻함 (현재는 성공한 관제 단계 -> 추후 회의를 통해 정확한 사용 방법 정해야 됨)
-    //defense : { type : progress, required : true },
     beActivated : { type : Array, required : true },
     defenseActive : { type : Array, required : true },
     defenseLv : { type : Array, required : true },
     defenseCnt : { type : Array, required : true },
-
-
 })
 
 module.exports = mongoose.model('Section', Section); 
